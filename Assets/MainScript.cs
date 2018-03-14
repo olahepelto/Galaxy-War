0a3ebee00a7899e78a810e337150f0e8ef71ce57
Otto Lähepelto <otto.lahepelto@gmail.com>
2018-03-14 20:25:07 +0200

    Import to version control.

---
+++ Assets/MainScript.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScript : MonoBehaviour {
+
+
+    private List<Node> nodeList = new List<Node>();
+    public Transform nodePrefab;
+
+    public LineFactory lineFactory;
+    private Line drawnLine;
+
+    void Start()
+    {
+        lineFactory = GameObject.FindGameObjectWithTag("LineFactory").GetComponent<LineFactory>();
+        nodePrefab = Resources.Load<Transform>("Node");
+        GenerateNodes();
+        GenerateNodeConnections();
+    }
+
+    public void RemoveNodeFromList(Node node)
+    {
+        Destroy(node.transformInstance);
+        nodeList.Remove(node);
+        node = null;
+    }
+
+    public void GenerateNodes()
+    {
+        for (int i = 0; i < 200; i++)
+        {
+            bool success = false;
+            while (!success)
+            {
+                float x = Random.Range(-9.0F, 9.0F);
+                float y = Random.Range(-4.0F, 4.0F);
+                float distance = GetDistanceToNearestNode(x, y);
+
+                if (distance > 1 && distance < 1.5 || true)
+                {
+                    nodeList.Add(new Node("Delta X-166 Epsilon", x, y));
+                    success = true;
+                }
+            }
+            
+        }
+    }
+
+    private void GenerateNodeConnections()
+    {
+        
+        foreach (Node node1 in this.nodeList)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Node node2 = this.nodeList[Random.Range(0, this.nodeList.Count - 1)];
+
+                float distance = GetDistanceBetweenNodes(node1, node2);
+
+                if (distance < 1.5 && node1.GetXPos() != node2.GetXPos() && node1.GetYPos() != node2.GetYPos())
+                {
+                    ConnectNodeToNode(node1, node2);
+                }
+            }
+        }
+        
+
+        foreach (Node node1 in this.nodeList)
+        {
+            if (node1.nodeConnectionsList.Count < 2)
+            {
+                Node[] closeNodes = GetClosestNode(node1);
+
+                ConnectNodeToNode(node1, closeNodes[0]);
+                ConnectNodeToNode(node1, closeNodes[1]);
+            }
+        }
+    }
+
+    private void ConnectNodeToNode(Node node1, Node node2)
+    {
+        drawnLine = lineFactory.GetLine(node1.transformInstance.position, node2.transformInstance.position, 0.01f, Color.gray);
+        node1.ConnectToNode(node2);
+        node2.ConnectToNode(node1);
+    }
+
+    public void ClearLines()
+    {
+        var activeLines = lineFactory.GetActive();
+
+        foreach (var line in activeLines)
+        {
+            line.gameObject.SetActive(false);
+        }
+    }
+
+    Node[] GetClosestNode(Node node1)
+    {
+        float smallestDistance = 100000.0F;
+
+        Node[] closeNodes = new Node[2] {node1,node1};
+
+        foreach (Node node2 in this.nodeList)
+        {
+            float nodeDistance = GetDistanceBetweenNodes(node1, node2);
+
+            if (nodeDistance < smallestDistance && nodeDistance != 0)
+            {
+                smallestDistance = nodeDistance;
+                closeNodes[1] = closeNodes[0];
+                closeNodes[0] = node2;
+                
+            }
+        }
+
+        return closeNodes;
+    }
+
+    float GetDistanceToNearestNode(float x, float y)
+    {
+        float smallestDistance = 100000.0F;
+
+        foreach (Node node in this.nodeList)
+        {
+            float x_distance = x - node.GetXPos();
+            float y_distance = y - node.GetYPos();
+            float nodeDistance = Mathf.Pow(x_distance, 2F) + Mathf.Pow(y_distance, 2F);
+            nodeDistance = Mathf.Sqrt(nodeDistance);
+
+            if (nodeDistance < smallestDistance && nodeDistance != 0)
+            {
+                smallestDistance = nodeDistance;
+            }
+        }
+
+        return smallestDistance;
+    }
+
+    float GetDistanceBetweenNodes(Node node1, Node node2)
+    {
+        float x_distance = node1.GetXPos() - node2.GetXPos();
+        float y_distance = node1.GetYPos() - node2.GetYPos();
+
+        float distance = Mathf.Pow(x_distance, 2F) + Mathf.Pow(y_distance, 2F);
+        distance = Mathf.Sqrt(distance);
+
+        return distance;
+    }
+
+	void Update () {
+        for(int i = 0; i < 20; i++)
+        {
+            foreach (Node node in nodeList)
+            {
+                node.Tick();
+            }
+        }
+	}
+}
---
+++ Assets/Node.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+    public Transform transformInstance;
+    private MainScript mainScript;
+    public List<Node> nodeConnectionsList = new List<Node>();
+
+    string systemName;
+    float xPos;
+    float yPos;
+
+    public int warships;
+    public int health = 10;
+    public int color;
+
+    public Node(string systemName, float xPos, float yPos)
+    {
+        mainScript = GameObject.FindGameObjectWithTag("ScriptObject").GetComponent<MainScript>();
+
+        warships = Random.Range(1, 10);
+        color = Random.Range(0, 2);
+
+        this.transformInstance = Instantiate(mainScript.nodePrefab, new Vector2(xPos, yPos), Quaternion.identity);
+        this.systemName = systemName;
+        this.xPos = xPos;
+        this.yPos = yPos;
+    }
+
+    public void RemoveNode()
+    {
+        mainScript.RemoveNodeFromList(this);
+    }
+    public float GetXPos()
+    {
+        return this.xPos;
+    }
+    public float GetYPos()
+    {
+        return this.yPos;
+    }
+    public void ConnectToNode(Node node)
+    {
+        nodeConnectionsList.Add(node);
+    }
+    public void RemoveNodeConnection(Node node)
+    {
+        nodeConnectionsList.Remove(node);
+    }
+    public void ClearNodeConnections()
+    {
+        nodeConnectionsList.Clear();
+    }
+
+    public void Tick()
+    {
+
+        if (color == 0)
+        {
+            this.transformInstance.GetComponent<SpriteRenderer>().color = new Color(1F, 0F, 0F, 1);
+        }
+        else if(color == 1)
+        {
+            this.transformInstance.GetComponent<SpriteRenderer>().color = new Color(0F, 0F, 1F, 1);
+        }
+
+        if (this.warships > 0 && Random.Range(1, 24) == 2)
+        {
+            Node randomPathNode = nodeConnectionsList[Random.Range(0, nodeConnectionsList.Count - 1)];
+
+            if (randomPathNode.color != this.color)
+            {
+                if(randomPathNode.health < 1)
+                {
+                    randomPathNode.color = this.color;
+                    randomPathNode.health = 10;
+                    randomPathNode.warships++;
+                    this.warships--;
+                }
+                else if (randomPathNode.health > 0 && randomPathNode.warships < 1)
+                {
+                    this.warships--;
+                    randomPathNode.health -= 2;
+                }
+                else if(randomPathNode.warships > this.warships)
+                {
+                    randomPathNode.warships -= this.warships / 2;
+                    this.warships = 0;
+                }
+                else if (randomPathNode.warships < this.warships)
+                {
+                    this.warships -= randomPathNode.warships / 2;
+                    randomPathNode.warships = 0;
+                }
+                else if (randomPathNode.warships == this.warships)
+                {
+                    this.warships = 0;
+                    randomPathNode.warships = 0;
+                }
+
+            }
+            else
+            {
+                randomPathNode.warships++;
+            }
+        }
+        if(Random.Range(1, 5) == 2)
+        {
+            this.warships++;
+        }
+    }
+}
